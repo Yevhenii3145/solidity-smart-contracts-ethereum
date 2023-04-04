2bb7220de38eddab80b3441f932fd1b818782086
Yevhenii3145 <zgardan19@ukr.net>
2023-04-04 22:42:02 +0300

    abc

---
+++ lesson_6_functions/Functions.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Demo {
+    // public
+    // external
+    // internal
+    // private
+    
+    function getBalance() public {}
+}
+
